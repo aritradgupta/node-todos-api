e1ab62f1b0ad100d79c9097930a37ca11872df66
aritradgupta <dattagupta.aritra@gmail.com>
2018-10-08 23:46:23 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -5,9 +5,7 @@ var {
   mongoose
 } = require('./db/mongoose');
 var Todo = require('./models/todo');
-var {
-  User
-} = require('./models/user');
+var User = require('./models/user');
 
 var app = express();
 
@@ -28,3 +26,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
