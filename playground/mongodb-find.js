86975cf29265c9018d7517bcb620c0994ecb2a0e
aritradgupta <dattagupta.aritra@gmail.com>
2018-10-08 14:39:55 +0530

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if (error) {
+    return console.log('Unable to connect to mongodb server.');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5bbb0c0548acff4562ea5fd7')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos');
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos');
+  // });
+
+  db.collection('Users').find({
+    name: 'Aritra'
+  }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos');
+  });  
+  //db.close();
+});
