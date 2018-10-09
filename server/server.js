2ea4c123127fec5387da63dd2cbc52108570112a
aritradgupta <dattagupta.aritra@gmail.com>
2018-10-09 15:29:44 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectId} = require('mongodb');
+var {
+  ObjectId
+} = require('mongodb');
 
 var {
   mongoose
@@ -27,7 +30,9 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({
+      todos
+    });
   }, (e) => {
     res.status(400).send(e);
   });
@@ -35,16 +40,18 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
-  if(!ObjectId.isValid(id)){
+  if (!ObjectId.isValid(id)) {
     return res.status(400).send();
   }
 
   Todo.findById(id).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(400).send();
     }
 
-    res.send({todo});
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send(e);
   });
@@ -52,22 +59,56 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
-  if(!ObjectId.isValid(id)){
+  if (!ObjectId.isValid(id)) {
     return res.status(404).send();
   }
-  
+
   Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo){
+    if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo});
+    res.send({
+      todo
+    });
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => res.status(400).send(e));
+
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
